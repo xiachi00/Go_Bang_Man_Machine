8b5bbc26595ea04dab92e2e4b7117531844dd3d4
xiachi00 <1323632882@qq.com>
2023-03-15 12:10:36 +0800

    删除无效文件夹resource

--- resource/reset_the_game.js
+++
@@ -1,6 +0,0 @@
-var result = confirm((player === 1 ? "黑棋" : "白棋") + '获胜！\n\n是否重启游戏？');
-if (result) {
-    location.href = "http://150.158.146.95:8888/";
-} else {
-    location.href = "http://150.158.146.95:8888/chess.html";
-}
